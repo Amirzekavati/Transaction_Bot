6bf548546e669e45db76087edf71ac6c0295e113
Amirz <amir.zekavati.a@gmail.com>
2025-02-06 00:32:57 +0330

    I add crawler for TSEMTMC and run successfully

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ from config import BOT_TOKEN
 from commands import get_handlers
 from monitor import check_stock_volumes
 
-def main():
+async def main():
     application = Application.builder().token(BOT_TOKEN).build()
 
     for handler in get_handlers():
         application.add_handler(handler)
 
+    await asyncio.create_task(check_stock_volumes())
+
     print("🤖 Bot is starting...")
-    application.run_polling()
+    await application.run_polling()
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
